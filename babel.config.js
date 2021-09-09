4477998d5676af009f7a24c9089ea43ac3d31463
CristianFigueredo <cristian28figueredo@gmail.com>
2021-09-09 12:08:30 -0400

    initial config

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,18 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
+module.exports = function (api) {
+    api.cache(true);
+    return {
+        presets: ['babel-preset-expo', '@babel/preset-typescript'],
+        plugins: [
+            [
+                'module-resolver',
+                {
+                    root: '.',
+                    extensions: ['.ts', '.tsx', '.json', '.web.tsx'],
+                    alias: {
+                        '~': './src',
+                    },
+                },
+            ],
+        ],
+    };
 };
